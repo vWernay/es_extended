cc2bcd4def4c21f0c1f86603b2756dba9d3fb2ce
D3v <39461646+deviljin112@users.noreply.github.com>
2020-06-07 19:44:59 +0100

    Ensure Licenses

--- modules/__core__/job-menu/client/events.lua
+++ modules/__core__/job-menu/client/events.lua
@@ -1,3 +1,15 @@
+-- Copyright (c) Jérémie N'gadi
+--
+-- All rights reserved.
+--
+-- Even if 'All rights reserved' is very clear :
+--
+--   You shall not use any piece of this software in a commercial product / service
+--   You shall not resell this software
+--   You shall not provide any facility to install this particular software in a commercial product / service
+--   If you redistribute this software, you must link to ORIGINAL repository at https://github.com/ESX-Org/es_extended
+--   This copyright should appear in every part of the project code
+
 local Input = M('input')
 
 local opened = false
@@ -6,5 +18,5 @@ Input.On('released', Input.Groups.MOVE, Input.Controls.SELECT_CHARACTER_FRANKLIN
 
     opened = not opened
     module.MenuStatus(opened)
-  
+
 end)
