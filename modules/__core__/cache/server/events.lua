83de1dc67028d2c293123723f6171fe7244802fe
Ark <arkseyonet@gmail.com>
2020-10-08 10:34:18 -0500

    Initiate centralized caching (unfinished, have to sleep)

--- modules/__core__/cache/server/events.lua
+++ modules/__core__/cache/server/events.lua
@@ -10,23 +10,71 @@
 --   If you redistribute this software, you must link to ORIGINAL repository at https://github.com/ESX-Org/es_extended
 --   This copyright should appear in every part of the project code
 
--- on('esx:startCache', function()
---   if Config.Modules.cache.cachedTables then
---     for k,v in pairs(Config.Modules.cache.cachedTables) do
---       MySQL.Async.fetchAll('SELECT * FROM ' .. v, {}, function(result)
---         if result then
---           for i=1, #result, 1 do
---             if i then
---               print("^3" .. v .. " - ^2RESULT ^1" .. i .. "^7")
---               for j,r in pairs(result[i]) do
---                 print(j .. " | " .. r)
---               end
---             end
---           end
---         else
---           print(false)
---         end
---       end)
---     end
---   end
--- end)
+on('esx:startCache', function()
+  if Config.Modules.cache.cachedTables then
+    for _,tab in pairs(Config.Modules.cache.cachedTables) do
+      if module.Cache[tab] == nil then
+        module.Cache[tab] = {}
+      end
+
+      MySQL.Async.fetchAll('SELECT * FROM ' .. tab, {}, function(result)
+        for _,data in ipairs(result) do
+          local index = #module.Cache[tab]+1
+          module.Cache[tab][index] = {}
+
+          for k,v in pairs(data) do
+            module.Cache[tab][index][k] = {}
+
+            if type(v) == "string" and v:len() >= 2 and v:find("{") and v:find("}") then
+              module.Cache[tab][index][k] = json.decode(v)
+            else
+              module.Cache[tab][index][k] = v
+            end
+          end
+        end
+      end)
+    end
+  end
+
+  if Config.Modules.cache.identityCachedTables then
+    for _,tab in pairs(Config.Modules.cache.identityCachedTables) do
+      if module.Cache[tab] == nil then
+        module.Cache[tab] = {}
+      end
+
+      MySQL.Async.fetchAll('SELECT * FROM ' .. tab, {}, function(result)
+        for i=1, #result, 1 do
+          if result[i].owner then
+            if not module.Cache[tab][result[i].owner] then
+              module.Cache[tab][result[i].owner] = {}
+            end
+
+            if not module.Cache[tab][result[i].owner][result[i].id] then
+              module.Cache[tab][result[i].owner][result[i].id] = {}
+            end
+
+            MySQL.Async.fetchAll('SELECT * FROM ' .. tab .. ' WHERE owner = @owner AND id = @id', {
+              ['@owner'] = result[i].owner,
+              ['@id']    = result[i].id
+            }, function(result2)
+              for _,data in ipairs(result) do
+                local index = #module.Cache[tab]+1
+                module.Cache[tab][result[i].owner][result[i].id][index] = {}
+
+                for k,v in pairs(data) do
+                  module.Cache[tab][result[i].owner][result[i].id][index][k] = {}
+
+                  if type(v) == "string" and v:len() >= 2 and v:find("{") and v:find("}") then
+                    module.Cache[tab][result[i].owner][result[i].id][index][k] = json.decode(v)
+                  else
+                    module.Cache[tab][result[i].owner][result[i].id][index][k] = v
+                  end
+                end
+              end
+            end)
+          end
+        end
+      end)
+    end
+  end
+end)
--- modules/__core__/cache/server/module.lua
+++ modules/__core__/cache/server/module.lua
@@ -10,3 +10,21 @@
 --   If you redistribute this software, you must link to ORIGINAL repository at https://github.com/ESX-Org/es_extended
 --   This copyright should appear in every part of the project code
 
+module.Cache = {}
+
+
+ESX.SetInterval(5000, function()
+  if module.Cache["owned_vehicles"] then
+    for k,v in pairs(module.Cache["owned_vehicles"]) do
+      for l,w in pairs(module.Cache["owned_vehicles"][k]) do
+        for m,x in pairs(module.Cache["owned_vehicles"][k][l]) do
+          for n,y in pairs(module.Cache["owned_vehicles"][k][l][m]) do
+            if y then
+              print(n .. " | " .. tostring(y))
+            end
+          end
+        end
+      end
+    end
+  end
+end)
