7841fe50e79dbc5927e3dd998e0bf8447dee4fc8
chip <onlygamernerd@gmail.com>
2020-10-30 23:27:57 +0100

    feat(status/server): server side functionality

--- modules/__core__/status/server/events.lua
+++ modules/__core__/status/server/events.lua
@@ -9,3 +9,48 @@
 --   You shall not provide any facility to install this particular software in a commercial product / service
 --   If you redistribute this software, you must link to ORIGINAL repository at https://github.com/ESX-Org/es_extended
 --   This copyright should appear in every part of the project code
+
+on('esx:playerLoaded', function(playerId, xPlayer)
+	MySQL.Async.fetchAll('SELECT status FROM users WHERE identifier = @identifier', {
+		['@identifier'] = xPlayer.identifier
+	}, function(result)
+		local data = {}
+
+		if result[1].status then
+			data = json.decode(result[1].status)
+		end
+
+		xPlayer.set('status', data)
+		TriggerClientEvent('esx_status:load', playerId, data)
+	end)
+end)
+
+on('esx:playerDropped', function(playerId, reason)
+	local xPlayer = ESX.GetPlayerFromId(playerId)
+	local status = xPlayer.get('status')
+
+	MySQL.Async.execute('UPDATE users SET status = @status WHERE identifier = @identifier', {
+		['@status']     = json.encode(status),
+		['@identifier'] = xPlayer.identifier
+	})
+end)
+
+onRequest('status:getStatus', function(playerId, statusName, cb)
+	local xPlayer = ESX.GetPlayerFromId(playerId)
+	local status  = xPlayer.get('status')
+
+	for i=1, #status, 1 do
+		if status[i].name == statusName then
+			cb(status[i])
+			break
+		end
+	end
+end)
+
+on('status:update', function(status)
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	if xPlayer then
+		xPlayer.set('status', status)
+	end
+end)
--- modules/__core__/status/server/main.lua
+++ modules/__core__/status/server/main.lua
@@ -9,3 +9,26 @@
 --   You shall not provide any facility to install this particular software in a commercial product / service
 --   If you redistribute this software, you must link to ORIGINAL repository at https://github.com/ESX-Org/es_extended
 --   This copyright should appear in every part of the project code
+
+module.SaveData()
+
+ESX.SetInterval(1000, function()
+	local players = ESX.GetPlayers()
+
+	for _,playerId in ipairs(players) do
+		local xPlayer = ESX.GetPlayerFromId(playerId)
+
+		MySQL.Async.fetchAll('SELECT status FROM users WHERE identifier = @identifier', {
+			['@identifier'] = xPlayer.identifier
+		}, function(result)
+			local data = {}
+
+			if result[1].status then
+				data = json.decode(result[1].status)
+			end
+
+			xPlayer.set('status', data)
+			emitClient('esx_status:load', playerId, data)
+		end)
+	end
+end)
