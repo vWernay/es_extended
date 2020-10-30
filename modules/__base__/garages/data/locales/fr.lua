fbaa50f529038cb5e8cec2d7d248653f29f41a40
Ark <arkseyonet@gmail.com>
2020-10-30 14:32:52 -0500

    Update french translation

--- modules/__base__/garages/data/locales/fr.lua
+++ modules/__base__/garages/data/locales/fr.lua
@@ -11,7 +11,28 @@
 --   This copyright should appear in every part of the project code
 
 Translations = {
-  ['returned_vehicles_to_garages_client'] = 'Returned all owned vehicles to their garages.',
-  ['returned_vehicles_to_garages_server'] = '^2returned all owned vehicles to their garages^7',
-  ['press_to_retrieve'] = 'Press ~INPUT_CONTEXT~ to get a vehicle from your garage.'
+  -- General
+  ['returned_vehicles_to_garages_client'] = 'Tous les véhicules possédés ont été renvoyé dans le garage',
+  ['returned_vehicles_to_garages_server'] = '^2Tous les véhicules possédés ont été renvoyé dans le garage^7',
+  ['press_to_retrieve'] = 'Appuyez sur ~INPUT_CONTEXT~ pour sortir un véhicule de votre garage.',
+  ['press_to_store'] = 'Appuyez sur ~INPUT_CONTEXT~ pour ranger ce véhicule dans le garage.',
+  ['must_be_in_vehicle'] = '~r~Vous devez être dans un véhicule.',
+  ['no_vehicles'] = 'Vous ne possédez aucun véhicule.',
+  ['not_in_garage'] = 'Votre %s avec la plaque %s n\'est pas dans le garage.',
+  ['model_error'] = 'Il y a eu une erreur avec le modèle de ce véhicule.',
+  ['plate_error'] = 'Il y a eu une erreur pour récuprérer la plaque de la voiture.',
+  ['retrieve_success'] = 'Vous avez sortit votre véhicule du garage.',
+  ['retrieve_failure'] = 'Impossible de récupérer votre véhicule. Veuillez réessayer..',
+  ['store_success'] = 'Nous avons rangé la voiture dans votre garage.',
+  ['store_failure'] = 'Impossible de ranger votre véhicule dans le garage. Veuillez réessayer..',
+  ['do_not_own'] = '~r~Ce véhicule ne vous appartient pas.',
+
+  -- Menu
+  ['menu_title'] = 'Garages',
+  ['exit'] = 'Sortie',
+  ['not_in_garage_label'] = "PAS DANS LE GARAGE",
+  ['model_error_label'] = "ERREUR DE MODÈLE",
+  ['yes'] = 'Oui',
+  ['no'] = 'Non',
+  ['retrieve_confirm'] = 'Récupérer %s du garage ?'
 }
