cfdfb377cedfcd293755e4f4e8f0b6e5a334dadb
chip <onlygamernerd@gmail.com>
2020-11-02 23:45:54 +0100

    feat(status/nui): fade for each status bar

--- src/ui/status/src/components/StatusCircle.tsx
+++ src/ui/status/src/components/StatusCircle.tsx
@@ -4,6 +4,9 @@ import CircularProgress from "@material-ui/core/CircularProgress";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import Fab from "@material-ui/core/Fab";
 import { useStatus } from "./hooks/useStatus";
+import Switch from '@material-ui/core/Switch';
+import Fade from '@material-ui/core/Fade';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -20,39 +23,49 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
+/*
+75%, 50%, 25%, 20, 15, 10, 5, 0
+And 100
+*/
+
 function StatusCircle(): any {
   const status = useStatus();
   const classes = useStyles();
+
+  let fadeValue = [100, 75, 50, 25, 15, 10, 5, 0]
+
   return (
     <div
       className="App"
       style={{ display: "flex", position: "absolute", right: 14, bottom: 20 }}
     >
       {status.map((stat) => (
-        <div key={stat.id} className={classes.wrapper}>
-          <Fab style={{ background: "#232323" }} size="medium">
-            {stat.iconType === "material" ? (
-              <i style={{ color: stat.color }} className="material-icons">
-                {stat.icon}
-              </i>
-            ) : (
-              <i
-                style={{ color: stat.color }}
-                className={`fa ${stat.icon}`}
-              ></i>
-            )}
-          </Fab>
-          <CircularProgress
-            style={
-              stat.value <= 10 ? { color: "#d63031" } : { color: stat.color }
-            }
-            variant="static"
-            size={52}
-            thickness={5}
-            value={stat.value}
-            className={classes.fabProgress}
-          />
-        </div>
+        <Fade in={fadeValue.includes(stat.value)}>
+          <div key={stat.id} className={classes.wrapper}>
+            <Fab style={{ background: "#232323" }} size="medium">
+              {stat.iconType === "material" ? (
+                <i style={{ color: stat.color }} className="material-icons">
+                  {stat.icon}
+                </i>
+              ) : (
+                <i
+                  style={{ color: stat.color }}
+                  className={`fa ${stat.icon}`}
+                ></i>
+              )}
+            </Fab>
+            <CircularProgress
+              style={
+                stat.value <= 10 ? { color: "#d63031" } : { color: stat.color }
+              }
+              variant="static"
+              size={52}
+              thickness={5}
+              value={stat.value}
+              className={classes.fabProgress}
+            />
+          </div>
+        </Fade>
       ))}
     </div>
   );
