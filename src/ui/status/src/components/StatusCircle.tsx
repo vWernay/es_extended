c101c1f463e98d99e6c510af67ee39b439fcd432
chip <onlygamernerd@gmail.com>
2020-11-08 23:23:23 +0100

    changes for status progressbar

--- src/ui/status/src/components/StatusCircle.tsx
+++ src/ui/status/src/components/StatusCircle.tsx
@@ -1,20 +1,21 @@
 import React, { useState } from "react";
-import "../App.css"
+import "../App.css";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import Fab from "@material-ui/core/Fab";
 import { useStatus } from "./hooks/useStatus";
-import Switch from '@material-ui/core/Switch';
-import Fade from '@material-ui/core/Fade';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from "@material-ui/core/Switch";
+import Fade from "@material-ui/core/Fade";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import { stat } from "fs";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     fabProgress: {
       position: "absolute",
       margin: 15,
-      left: -17,
-      bottom: -17,
+      left: -19.6,
+      bottom: -20,
     },
     wrapper: {
       margin: theme.spacing(1),
@@ -23,26 +24,31 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
-/*
-75%, 50%, 25%, 20, 15, 10, 5, 0
-And 100
-*/
-
 function StatusCircle(): any {
   const status = useStatus();
   const classes = useStyles();
 
-  let fadeValue = [100, 75, 50, 25, 20, 15, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+  let fadeValue = [100, 75, 50, 25, 20, 15, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
 
   return (
     <div
       className="App"
       style={{ display: "flex", position: "absolute", right: 14, bottom: 20 }}
+
     >
       {status.map((stat) => (
-        <Fade in={fadeValue.includes(stat.value)}>
+        <Fade
+          in={
+            stat.fadeType === "asc" && stat.value > 0
+              ? true :
+            stat.fadeType === "desc"
+              ? fadeValue.includes(stat.value) 
+              : stat.fadeType === "desc" && stat.value === 0 
+              ? false : false
+          }
+        >
           <div key={stat.id} className={classes.wrapper}>
-            <Fab style={{ background: "#232323" }} size="medium">
+            <Fab style={{ background: "#232323" }} size="small">
               {stat.iconType === "material" ? (
                 <i style={{ color: stat.color }} className="material-icons">
                   {stat.icon}
@@ -56,10 +62,14 @@ function StatusCircle(): any {
             </Fab>
             <CircularProgress
               style={
-                stat.value <= 10 ? { color: "#d63031" } : { color: stat.color }
+                stat.value <= 10 && stat.fadeType === "desc"
+                  ? { color: "#d63031" }
+                  : stat.fadeType === "asc" && stat.value >= 90
+                  ? { color: "#d63031" }
+                  : { color: stat.color }
               }
               variant="static"
-              size={52}
+              size={50}
               thickness={5}
               value={stat.value}
               className={classes.fabProgress}
